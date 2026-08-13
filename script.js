27cc56532dd809e3e14bd5b41c12455a3836749b
RAMITHA HATWAR <ramithahatwar76@gmail.com>
2026-08-13 13:43:25 +0530

    Add script.js and implement dynamic time-based hero greeting

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("javascript is connected");
+
+let year=new Date().getFullYear();
+document.querySelector(".site-footer p").innerHTML=
+`&copy; ${year} ramitha hatwar. All rights reserved`;
+
+function getGreeting(){
+    const hour=new Date().getHours();
+    if(hour<12) return"Good Morning";
+    if(hour<17) return"Good Afternoon";
+    return"Good Evening"
+}
+let hero=document.querySelector(".hero-section h1")
+if(hero){
+    hero.textContent=`${getGreeting()},I'm Ramitha Hatwar 👋🏻`;
+}
+
